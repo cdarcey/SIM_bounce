1efcc07182adcfb265d8df134fe316c8fb8e915a
Carey <careydarcey@rocketmail.com>
2026-04-30 09:44:40 -0500

    WIP

--- RUN_test/input.py
+++ RUN_test/input.py
@@ -1,12 +1,18 @@
 
 
+trick.var_server_set_port(49152)
+trick.var_server_set_enabled(1)
+
+trick.real_time_enable()
+trick.exec_set_software_frame(0.1)
+
 dr = trick.DRAscii("ball")
 dr.add_variable("ball_sim.ball.dPosition")
 dr.add_variable("ball_sim.ball.dVelocity")
 dr.set_freq(trick.DR_Always)
 trick.add_data_record_group(dr)
 
-ball_sim.ball.dPosition = 500.0
+ball_sim.ball.dPosition = 50.0
 ball_sim.ball.dVelocity = 0.0
 ball_sim.ball.dGravity = 9.81
 ball_sim.ball.dBounce = 0.8
